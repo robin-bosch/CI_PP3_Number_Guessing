19712407534f8898d98072545f6aabab3abbc0ea
alphaClass6143 <alphaClass@web.de>
2022-11-16 23:38:23 +0100

    Added show rules function and clear console

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 DIFFICULTIES = {
     "easy": {
@@ -25,18 +26,43 @@ DIFFICULTIES = {
 
 regex = "^[1-5]{1}$"
 
+def show_rules(showAfter):
+    '''
+    Displays rules than switches back to given screen after enter
+    '''
+    print("The rules")
+    input("Press Enter to continue...")
+    showAfter()
+    clear_console()
+
+#Code used: https://stackoverflow.com/questions/517970/how-to-clear-the-interpreter-console
+def clear_console():
+    '''
+    Clears console
+    '''
+    os.system('cls' if os.name=='nt' else 'clear')
+
+
+
 def main_menu():
 
-    print("Welcome to NumberGuessing\n")
-    print("Select one option:")
-    print("1. Help")
-    print("2. Rules")
-    print("3. Settings")
-    print("4. Start game")
-    print("5. Exit")
+    print('''
+Welcome to NumberGuessing
+
+Select one option:
+1. Help
+2. Rules
+3. Settings
+4. Start game
+5. Exit
+    ''')
     while True:
         option = input("Select: ")
         if re.search(regex, option):
+            match option:
+                case "2":
+                    show_rules(main_menu)
+                    break
             break
         else:
             print("Please select the correct option")
