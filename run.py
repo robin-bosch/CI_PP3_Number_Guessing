aa1dc42ab66544e261753a0c32c7eeb1826fbdd4
alphaClass6143 <alphaClass@web.de>
2022-11-17 14:57:54 +0100

    Added connection to spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 import re
 import os
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('number_guessing')
+
+
 
 class Difficulty():
     '''
